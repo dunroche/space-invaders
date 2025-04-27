f727074922c06ed696b9e75011dd8b40f7ef66e0
dunroche <dunroche@gmail.com>
2025-04-27 12:25:26 -0400

    updated: circleshape.py, constants.py, main.py, player.py

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import pygame
 from constants import *
+from player import Player
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS
 
 def main():
   pygame.init()
+  clock = pygame.time.Clock()
+  dt = 0  # initialize dt
+
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   print("Starting Asteroids!")
   print("Screen width: 1280")
   print("Screen height: 720")
 
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+  running = True
   while True:
-      screen.fill((0, 0, 0))  # fill the screen with black
-      pygame.display.flip()   # update the full display
-      for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-          return
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        running = False
+
+    screen.fill((0, 0, 0))  # fill the screen with black
+    player.draw(screen)
+    pygame.display.flip()   # update the full display
+
+    dt = clock.tick(60) / 1000
+  pygame.quit()
+
 if __name__ == "__main__":
    main()
